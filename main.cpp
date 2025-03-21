2cb960a6d22964f41151335f5c164c7d5bc44b7d
Alessio Giay <s310703@studenti.polito.it>
2025-03-21 10:40:18 +0100

    Aggiunta file function.hpp

--- main.cpp
+++ main.cpp
@@ -1,62 +1,6 @@
 #include <iostream>
 #include <iomanip>
-
-template <typename T>
-
-class NumCompx 
-{
-	private:
-		T real;
-		T imag;
-		
-	public:
-		
-	NumCompx() : real(0), imag(0) {}  //Default
-	NumCompx(T r, T i) : real(r), imag(i) {}  //Parametri
-	
-	//Coniugato
-	NumCompx coniugato() const{ 
-		return NumCompx(real,-imag);}
-	
-	//Parte reale e immaginaria
-	T ParteReale() const { 
-		return(real);}
-	T ParteImmaginaria () const { 
-		return(imag);}
-	
-	//Overload +
-	NumCompx operator+(const NumCompx& other) {
-		return NumCompx(real + other.real, imag + other.imag);}
-
-	//Overload *
-	NumCompx operator*(const NumCompx& other) {
-		return NumCompx(real * other.real - imag * other.imag,
-			   			real * other.imag + imag * other.real);}
-			  
-	//Overload +=
-	NumCompx operator+=(const NumCompx& other) {
-		real += other.real;
-		imag += other.imag;
-		return *this;}
-		
-	//Overload *=
-	NumCompx operator*=(const NumCompx& other) {
-		T NewR = real * other.real - imag * other.imag;
-		T NewI = real * other.imag + imag * other.real;
-		real = NewR;
-		imag = NewI;
-		return *this;}
-			 
-	// Overload dell'operatore <<
-    friend std::ostream& operator<<(std::ostream& os, const NumCompx& c) {
-        os << c.real;
-        if (c.imag >= 0)
-            os << "+" << c.imag << "i";
-        else
-            os << c.imag << "i";
-        return os;
-    }
-};
+#include "function.hpp"
 
 int main()
 {	
