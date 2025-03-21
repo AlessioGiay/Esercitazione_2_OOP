2491575838dd5eb4bea0540f416e13a3943fa0cb
Alessio Giay <s310703@studenti.polito.it>
2025-03-21 08:04:50 +0100

    Aggiornamento main

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-using namespace std;
+#include <iomanip>
 
 template <typename T>
 
@@ -15,19 +15,53 @@ class NumCompx
 	NumCompx(T r, T i) : real(r), imag(i) {}  //Parametri
 	
 	//Coniugato
-	NumCompx coniugato() const{ return NumCompx(real,-imag);}
+	NumCompx coniugato() const{ 
+		return NumCompx(real,-imag);}
 	
 	//Parte reale e immaginaria
-	T ParteReale() const { return(real);}
-	T ParteImmaginaria () const { return(imag);}
+	T ParteReale() const { 
+		return(real);}
+	T ParteImmaginaria () const { 
+		return(imag);}
+	
+	//Overload +
+	NumCompx operator+(const NumCompx& other) {
+		return NumCompx(real + other.real, imag + other.imag);}
+
+	//Overload *
+	NumCompx operator*(const NumCompx& other) {
+		return NumCompx(real * other.real - imag * other.imag,
+			   real * other.imag + imag * other.real);}
+			 
+	// Overload dell'operatore <<
+    friend std::ostream& operator<<(std::ostream& os, const NumCompx& c) {
+        os << c.real;
+        if (c.imag >= 0)
+            os << "+" << c.imag << "i";
+        else
+            os << c.imag << "i";
+        return os;
+    }
 };
 
 int main()
-{
+{	
 	NumCompx<double> z1(1.0, 2.0);
-	cout<<"Parte reale : "<<z1.ParteReale()<<endl;
-	cout<<"Parte immaginaria : "<<z1.ParteImmaginaria()<<endl;
-	//cout<<"Coniugato : "<<z1.coniugato()<<endl;
+	NumCompx<double> z2(3.3, 1.9);
+	
+	NumCompx<double> sum = z1 + z2; //ComplexNumber sum = operator+(c1, c2);
+    NumCompx<double> product = z1 * z2;
+    
+    std::cout << std::fixed << std::setprecision(1);
+    
+	std::cout<<"Parte reale : "<<z1.ParteReale()<<std::endl;
+	std::cout<<"Parte immaginaria : "<<z1.ParteImmaginaria()<<std::endl;
+	std::cout<<"Coniugato : "<<z1.coniugato()<<std::endl;
+	std::cout << "c1: " << z1 << "\n";
+    std::cout << "c2: " << z2 << "\n";
+    std::cout << "Somma: " << sum << "\n";
+    std::cout << "Prodotto: " << product << "\n";
+	
 	
 	return 0;
 }
