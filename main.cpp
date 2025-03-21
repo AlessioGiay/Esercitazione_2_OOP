8ab8c18566e36b818940abf7ac456a78b5afaf48
Alessio Giay <s310703@studenti.polito.it>
2025-03-21 09:31:58 +0100

    Aggiornamento main

--- main.cpp
+++ main.cpp
@@ -31,7 +31,21 @@ class NumCompx
 	//Overload *
 	NumCompx operator*(const NumCompx& other) {
 		return NumCompx(real * other.real - imag * other.imag,
-			   real * other.imag + imag * other.real);}
+			   			real * other.imag + imag * other.real);}
+			  
+	//Overload +=
+	NumCompx operator+=(const NumCompx& other) {
+		real += other.real;
+		imag += other.imag;
+		return *this;}
+		
+	//Overload *=
+	NumCompx operator*=(const NumCompx& other) {
+		T NewR = real * other.real - imag * other.imag;
+		T NewI = real * other.imag + imag * other.real;
+		real = NewR;
+		imag = NewI;
+		return *this;}
 			 
 	// Overload dell'operatore <<
     friend std::ostream& operator<<(std::ostream& os, const NumCompx& c) {
@@ -48,20 +62,24 @@ int main()
 {	
 	NumCompx<double> z1(1.0, 2.0);
 	NumCompx<double> z2(3.3, 1.9);
-	
-	NumCompx<double> sum = z1 + z2; //ComplexNumber sum = operator+(c1, c2);
-    NumCompx<double> product = z1 * z2;
-    
+
     std::cout << std::fixed << std::setprecision(1);
     
+	std::cout << "z1: " << z1 << "\n";
 	std::cout<<"Parte reale : "<<z1.ParteReale()<<std::endl;
 	std::cout<<"Parte immaginaria : "<<z1.ParteImmaginaria()<<std::endl;
-	std::cout<<"Coniugato : "<<z1.coniugato()<<std::endl;
-	std::cout << "c1: " << z1 << "\n";
-    std::cout << "c2: " << z2 << "\n";
+	std::cout<<"Coniugato : "<<z1.coniugato()<<std::endl;	
+	
+	NumCompx<double> sum = z1 + z2;
+    NumCompx<double> product = z1 * z2;
+	NumCompx<double> Rsum = z1 += z2;
+	NumCompx<double> Rproduct = z1 *= z2;
+	
+    std::cout << "z2: " << z2 << "\n";
     std::cout << "Somma: " << sum << "\n";
     std::cout << "Prodotto: " << product << "\n";
-	
+    std::cout << "Somma +=: " << Rsum << "\n";
+    std::cout << "Prodotto *=: " << Rproduct <<"\t\"Ora z1 vale " << Rsum << "\"\n";	
 	
 	return 0;
 }
